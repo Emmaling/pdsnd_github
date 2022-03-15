079e8af23f140d9487946c79bdff56ca1cee6c49
Emma Qin <512953925@qq.com>
2022-03-15 11:01:26 -0400

    Indicate the time related function

--- WIP_Bikeshare.py
+++ WIP_Bikeshare.py
@@ -62,7 +62,7 @@ def load_data(city, month, day):
 
     return df
 
-
+#This will be a time related function
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
